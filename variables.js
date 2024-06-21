0c4272ef292df6fd54d7c8e4af8705ff5abdb6f6
Ankit Kumar <ankitmallik33@gmail.com>
2024-06-21 20:50:49 +0530

    learned variable

---
+++ variables.js
@@ -0,0 +1,13 @@
+//can be declared with var let const
+//                 var     let      const
+// redeclare        1       0        0      
+// Reassign         1       1        0
+// Re-Initilized    1       1        0
+
+// var a = 10;
+// a = 20;
+// let b = 20;
+// let b = 10;
+
+// const pi = 3.14;
+// pi = 22/7;
