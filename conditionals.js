4663e849ff5e536483a7caeed055add73990bef6
Ankit Kumar <ankitmallik33@gmail.com>
2024-06-22 13:09:55 +0530

    learned switch

---
+++ conditionals.js
@@ -0,0 +1,87 @@
+// if (true) {
+//   console.log("hey");
+// } else {
+//   console.log("hey1");
+// }
+
+// if (5 == 0) {
+//   console.log("hey1");
+// } else if (7 == 7) {
+//   console.log("hey2");
+// } else if (1 == 1) {
+//   console.log("hey3");
+// } else {
+//   console.log("hey4");
+// }
+
+// switch (false) {
+//   case false:
+//   case "b": {
+//     console.log("a");
+//   }
+//   case "b": {
+//     console.log("b");
+//   }
+//   case "c": {
+//     console.log("c");
+//   }
+//   default: {
+//     console.log("d");
+//   }
+// }
+
+// const personInfo = {
+//   firstName: "pratyush",
+//   lastName: "karn",
+//   age: 22,
+// };
+
+// if (!personInfo.firstName) {
+
+// } else if (!personInfo.lastName) {
+
+// }
+// else if(!personInfo.age)
+// {
+
+// }
+// switch (personInfo)
+// {
+//     case !personInfo.firstName:
+//     {
+//         console.log('empty')
+//     }
+//     case !personInfo.lastName:
+//         {
+//           console.log('empty');
+//         }
+//         default:
+
+//         {
+//           console.log('no data found');
+//         }
+// }
+
+// const a = 5;
+// if(!(a==3)||true)
+//   {
+//     console.log('true');
+//   }
+//   else
+//   {
+//     console.log('false');
+//   }
+
+switch ("chandn") {
+  case "ankit": {
+    console.log("true");
+    break;
+  }
+  case "chandan": {
+    console.log("false");
+    break;
+  }
+  default: {
+    console.log("not found");
+  }
+}
---
+++ operator.js
@@ -0,0 +1,39 @@
+// Arithmetic Operators -> + - * / % 
+// Assignment Operators =
+//Comparison Operators  ==  === <= >= < > != !===
+//Logical Operators && || !
+
+// const val = (5==0) && (7==7);
+// console.log(val);
+// const val1 = ((5==9 || 0==0) && (7==0 && 7==7)) || true;
+
+// if(((5==9 || 0==0) && (7==0 && 7==7)) || false)
+// {
+//     console.log('Hello World !!');
+// }
+
+// if(1=='1')
+// {
+//     console.log('val1');
+// }
+
+// if(!true)
+// {
+//     console.log("hey");
+// }
+
+// if(        !((5==0 && 5=='5') && (true && false) )        )
+// {
+//     console.log('hey');
+// }
+
+// if (!(7 && false)) {
+//   console.log("hey");
+// }
+
+// if (!0) {
+//     console.log('hey');
+// }
+
+
+
